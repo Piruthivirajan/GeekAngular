901318151e1576cf67d9000d8f9938f7a28fd79e
Piruthivirajan <piruthivi32@gmail.com>
2017-06-15 15:50:40 +0530

    Create Server.js

--- Server.js
+++ Server.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const app=express();
-app.use(express.static(__dirname+'/dest'));
+app.use((req, res)=>res.sendfile(__dirname + '/public/index.html'));
+//app.use(express.static(__dirname+'/dest'));
+
 app.listen(process.env.PORT || 8080);
 
